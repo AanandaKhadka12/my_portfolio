3889e55cf4ba5b31edec318039d10ee7dd4b55a5
AanandaKhadka12 <aanandakhadka12@gmail.com>
2023-10-11 20:22:39 -0400

    v6

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -11,7 +11,7 @@ const Experience = () => {
       </p>
       <div className="grid grid-cols-1 max-w-[900px] mx-auto p-5">
         <div className="container mx-auto w-full h-full">
-          <div className="relative wrap overflow-hidden p-10 h-full">
+          <div className="relative wrap overflow-hidden md:p-10 sm:p-10 h-full">
             <div
               className="border-2-2 absolute border-opacity-20 border-gray-700 h-full border"
               style={{ left: "50%" }}
@@ -38,14 +38,14 @@ const Experience = () => {
                       <IoSchoolOutline />
                     </h1>
                   </div>
-                  <div className="order-1 rounded-lg shadow-xl w-5/12 px-6 py-4">
+                  <div className="order-1 rounded-lg shadow-xl w-5/12 md:px-6 md:py-4 sm:px-6 sm:py-4 px-2 py-2">
                     <h3 className="mb-3 font-bold text-gray-800 md:text-xl sm:text-lg text-md">
                       {item.title}
                     </h3>
                     <h4 className="mb-2 font-bold text-gray-800">
                       {item.location}
                     </h4>
-                    <p className="text-sm leading-snug tracking-wide text-gray-900 text-opacity-100">
+                    <p className="text-sm leading-snug tracking-wide text-gray-900 text-opacity-100 text-justify">
                       {item.desc}
                     </p>
                   </div>
